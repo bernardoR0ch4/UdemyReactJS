f91c52ec02c70a2c80760f1a5c990749a1bdb5e0
Bernardo Goncalves Rocha <bernardo.grocha@yahoo.com.br>
2022-08-09 15:08:38 -0300

    Template Expressions

---
+++ 2_FUNDAMENTOS/fundamentos/src/components/TemplateExpressions.js
@@ -0,0 +1,12 @@
+const TemplateExpressions = () => {
+    const name = "Bernardo";
+
+    return (
+<div>
+    <h1>Olá {name}, tudo bem?</h1>
+</div>
+
+    );
+};
+
+export default TemplateExpressions;
