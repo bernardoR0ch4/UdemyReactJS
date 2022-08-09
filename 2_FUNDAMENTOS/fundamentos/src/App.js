c65b5399ec9901089be4da66b1fe60eed7409688
Bernardo Goncalves Rocha <bernardo.grocha@yahoo.com.br>
2022-08-09 15:24:02 -0300

    MyComponent

--- 2_FUNDAMENTOS/fundamentos/src/App.js
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -2,12 +2,13 @@ import FirstComponent from './components/FirstComponent';
 import './App.css';
 import TemplateExpressions from './components/TemplateExpressions';
 
+
 function App() {
   return (
     <div className="App">
       <h1>Fundamentos React</h1>
-      <FirstComponent/>
-      <TemplateExpressions/>
+      <FirstComponent />
+      <TemplateExpressions />
     </div>
   );
 }
