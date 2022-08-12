05b5483f03d753d4949b1add795a76f9aa340b22
Bernardo Goncalves Rocha <bernardo.grocha@yahoo.com.br>
2022-08-12 14:09:01 -0300

    renderização condicional

--- 3_AVANCANDO_EM_REACT/avancando/src/components/ConditionalRender.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/ConditionalRender.js
@@ -4,12 +4,25 @@ import { useState } from "react";
 const ConditionalRender = () => {
     const [x] = useState(false);
 
+    const [name, setName] = useState("Bernardo")
+
 
   return (
     <div>
         <h1>Isso será exibido?</h1>
         {x && <p>Se x for true, sim!</p>}
         {!x && <p>Agora x é falso!</p>}
+        <h1>If ternário</h1>
+        {name === "João" ? (
+            <div>
+                <p>O nome é João</p>
+            </div>
+        ) : (
+            <div>
+                <p>Nome não é João</p>
+            </div>
+        )}
+        <button onClick={() => setName("João")}>Clique aqui!</button>
     </div>
   );
 };
