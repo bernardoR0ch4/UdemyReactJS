812a3a077bfe196a3f91d94f914f2467a564b3f7
Bernardo Goncalves Rocha <bernardo.grocha@yahoo.com.br>
2022-09-05 17:08:47 -0300

    css inline

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -5,8 +5,12 @@ function App() {
   return (
     <div className="App">
       <h1>React com CSS</h1>
-      <MyComponent/>
+      <MyComponent />
       <p>Este parágrafo é do App.js</p>
+      {/* Não é legal utilizar css inline */}
+      <p style={{ color: "blue", padding: "25px", borderTop: "2px solid red" }}>
+        Este elemento foi estilizad de forma inline
+      </p>
     </div>
   );
 }
