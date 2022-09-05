4485910eaae8cbebba76b3a83a0ea3035c70e623
Bernardo Goncalves Rocha <bernardo.grocha@yahoo.com.br>
2022-09-05 16:59:52 -0300

    CSS de componentes

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 function App() {
   return (
     <div className="App">
       <h1>React com CSS</h1>
+      <MyComponent/>
+      <p>Este parágrafo é do App.js</p>
     </div>
   );
 }
