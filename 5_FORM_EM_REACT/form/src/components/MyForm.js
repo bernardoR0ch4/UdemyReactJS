c46480cc878c9eba4d747f817f3c8e1abcd311e6
Bernardo Goncalves Rocha <bernardo.grocha@yahoo.com.br>
2022-09-06 18:08:06 -0300

    label envolvendo input

--- 5_FORM_EM_REACT/form/src/components/MyForm.js
+++ 5_FORM_EM_REACT/form/src/components/MyForm.js
@@ -8,6 +8,11 @@ const MyForm = () => {
                 <label htmlFor="name">Nome: </label>
                 <input type="text" name="name" placeholder="Digite o seu nome"/>
             </div>
+            {/* label envolvendo input */}
+            <label>
+                <span>E-mail</span>
+                <input type="email" name="email" placeholder="Digite seu email" />
+            </label>
             <input type="submit" value="Enviar" />
         </form>
     </div>
