a66e9b041443efde7500f987b4200492f615222a
Bernardo Goncalves Rocha <bernardo.grocha@yahoo.com.br>
2022-09-06 18:23:10 -0300

    alteração de state inline

--- 5_FORM_EM_REACT/form/src/components/MyForm.js
+++ 5_FORM_EM_REACT/form/src/components/MyForm.js
@@ -1,9 +1,9 @@
 import './MyForm.css'
 
-import {useState} from 'react'
+import { useState } from 'react'
 
 const MyForm = () => {
-// gerenciamento de dados
+    // gerenciamento de dados
     const [name, setName] = useState();
     const [email, setEmail] = useState();
 
@@ -12,29 +12,35 @@ const MyForm = () => {
     };
 
     console.log(name);
+    console.log(email);
 
 
-  return (
-    <div>
-        <form>
-            <div>
-                <label htmlFor="name">Nome: </label>
-                <input 
-                type="text" 
-                name="name" 
-                placeholder="Digite o seu nome"
-                onChange={handleName}
-                />
-            </div>
-            {/* label envolvendo input */}
-            <label>
-                <span>E-mail</span>
-                <input type="email" name="email" placeholder="Digite seu email" />
-            </label>
-            <input type="submit" value="Enviar" />
-        </form>
-    </div>
-  )
+    return (
+        <div>
+            <form>
+                <div>
+                    <label htmlFor="name">Nome: </label>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Digite o seu nome"
+                        onChange={handleName}
+                    />
+                </div>
+                {/* label envolvendo input */}
+                <label>
+                    <span>E-mail</span>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Digite seu email"
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </label>
+                <input type="submit" value="Enviar" />
+            </form>
+        </div>
+    )
 }
 
 export default MyForm
